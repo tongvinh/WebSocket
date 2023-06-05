7b9496b2420659c61298b567eb9ad34a3d57b87b
Vinh Tong <vinhtong.dev@gmail.com>
2023-06-05 13:47:20 +0700

    Build web socket with golang

---
+++ cmd/web/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"ws/internal/handlers"
+)
+
+func main() {
+	mux := routes()
+	log.Println("Starting channel listener")
+	go handlers.ListenToWsChannel()
+
+	log.Println("Starting web server on 8080")
+
+	_ = http.ListenAndServe(":8080", mux)
+}
